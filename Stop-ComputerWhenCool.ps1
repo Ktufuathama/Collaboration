48d6bc8326d6edddd24616ad4a4126eabda13f6b
Caleb Worthington <31010254+Ktufuathama@users.noreply.github.com>
2024-08-14 12:22:14 -0700

    Create Stop-ComputerWhenCool.ps1

---
+++ Stop-ComputerWhenCool.ps1
@@ -0,0 +1,20 @@
+#function Stop-ComputerWhenCool {
+  [cmdletbinding()]
+  param(
+    [int]$TempThreshold=55 # In Celsius (122F).
+  )
+  ### Main
+  while ($true) {
+    $TempCurrent=$((nvidia-smi.exe -q -a) -match "GPU Current Temp.*:(.*)").split(" ")[-2]
+    if ($TempCurrent -gt $TempThreshold) {
+      [console]::writeLine("GPU Current Temp ($($TempCurrent)) above $($TempThreshold) threshold. Waiting 5 seconds...")
+      Start-Sleep -seconds 5
+    }
+    else {
+      [console]::writeLine("GPU Current Temp below $($TempThreshold) threshold. Stopping computer...")
+      Start-Sleep -seconds 1
+      Stop-Computer -force -verbose
+      return
+    }
+  }
+#}
